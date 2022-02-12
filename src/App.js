4d050562ebddc01f316106471d68a3a272f7fcfd
Toni <tonimatias96@gmail.com>
2022-02-12 16:32:33 +0200

    added counter

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [type, setType] = useState("");
   const [activity, setActivity] = useState("")
+  const [counter, setCounter] = useState(0)
 
   /*  useEffect(() => {
      axios.get(URL)
@@ -20,9 +21,12 @@ function App() {
  
    }, [])  */
 
-  function getData(e) {
-    e.preventDefault();
+  
+
+  function getData(/* e */) {
+    /* e.preventDefault(); */
     document.getElementById("whatActivity").style = "block";
+    document.getElementById("clicks").style = "block";
 
     axios.get(URL)
       .then((response) => {
@@ -31,36 +35,39 @@ function App() {
       }).catch(error => {
         alert(error)
       })
-
+      handleClick();
+       /* clickCounter(); */
   }
-  /* async function getData() {
-    
-    try {
-      const response = await fetch(URL);
 
-      if (response.ok) {
-        const json = await response.json();
-        setActivity(json.data.activity)
-        setType(json.data.type)
-      } else {
-        alert("Error!")
-      }
-    }catch (err) {
-      alert(err);
-    }
+/*   if (document.querySelector("h3").textContent === "") {
+    document.querySelector("#button").textContent = "Let's see"
+  } else {
+    document.querySelector("#button").textContent = "Give me another one"
+  }
+ */
+  const handleClick = () => {
+    setCounter(counter + 1)
+  }
+  /* let clicks = 0;
+  function clickCounter() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
   } */
 
   return (
     <div id='container'>
+      
       <header>
-        <h1>Bored? Here is something to do</h1>
+        <h1>Bored? Here's something to do</h1>
       </header>
       <div>
         <h3>{activity}</h3>
-        <p id='whatActivity' style={{display:"none"}}> What kind a activity this is:</p>
+        <p id='whatActivity' style={{display:"none"}}> What kind of activity is this:</p>
         <p>{type}</p>
-        <button onClick={getData}>Gimme another one</button>
+        <button id='button' onClick={getData}>Give me another one</button>
+        <p id='clicks' style={{display:"none"}}>Nothing interesting?  You pressed "Give me another one" button: {counter} times</p>
       </div>
+     
     </div>
   );
 }
